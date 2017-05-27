dae7a6795cfcbc49d9c7147c78cb28c202213bb2
Roy Pfaffman <roy.pfaffman@vevo.com>
2017-05-26 17:29:27 -0700

    Adds phantom and zombie tests

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const server = require('./lib/server')
 const db = require('./lib/db')
-const IntegrationTest = require('./lib/jobs/integration-test')
+const TestRunner = require('./lib/jobs/test-runner')
 
 db.connect()
   .then(() => db.migrate())
   .then(() => {
-    const test = new IntegrationTest()
-    test.run()
+    // const files = [ '/lib/tests/nightmare.js' ]
+    // const files = [ '/lib/tests/zombie.js' ]
+    const files = [ '/lib/tests/phantom.js' ]
+    const job = new TestRunner({ files })
+    job.run()
   })
   .then(() => server.start())
   .catch(e => { console.log("Uncaught error:", e.message, e.stack) })
--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 const defaults = {
   DB_URI: 'mongodb://localhost:27017/squire',
   HEADLESS: false,
-  KR_TEST_URI: 'http://localhost:8080',
+  KR_TEST_URI: 'http://firetv.vevo.com',
   PORT: 3000,
   TEST_TIMEOUT: 120000,
   VEVO_TEST_EMAIL: 'vevo.test.samsung@gmail.com',
